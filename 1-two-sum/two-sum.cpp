9df011595925372503c606adbf75d9fe36ba182b
Rakesh Kumar <rakeshkumar76537@gmail.com>
2025-06-17 22:13:39 +0530

    Time: 3 ms (71.36%) | Memory: 14.8 MB (51.47%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int, int> mp;
         for (int i = 0; i < nums.size(); i++) {
             int req = target - nums[i];
-            if (mp.find(req) != mp.end()) {
+            if (mp.count(req)) {
                 return {mp[req], i};
             }
             mp[nums[i]] = i;
