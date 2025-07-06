10cd987bff01306d5a455885f1684cf2ac59f241
Rakesh Kumar <rakeshkumar76537@gmail.com>
2025-07-06 22:00:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (81.20%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -3,7 +3,7 @@ public:
     void solve(int index, vector<int>& nums, vector<int>& temp, vector<vector<int>>& res){
         res.push_back(temp);
         for(int i=index;i<nums.size();i++){
-            if (i > index && nums[i] == nums[i - 1]) continue;
+            
             temp.push_back(nums[i]);
             solve(i+1,nums,temp,res);
             temp.pop_back();
