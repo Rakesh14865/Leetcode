5c38d44c0136ff2a7b934d1bf7381769148b5eb8
Rakesh Kumar <rakeshkumar76537@gmail.com>
2025-12-08 20:06:18 +0530

    Time: 8 ms (50.66%) | Memory: 7.7 MB (84.51%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     int countTriples(int n) {
         int res=0;
+        int k=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                for(int k=1;k<=n;k++){
-                    if((i*i)+(j*j)==(k*k)){
-                        res++;
-                    }
+                k=sqrt((i*i)+(j*j));
+                if((k*k)==((i*i)+(j*j)) && k<=n){
+                    res++;
+                }
+                
                 }
             }
+            return res;
         }
-    return res;
-    }
-};
+    
+    };
